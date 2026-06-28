5b2cdcb525897a993d6c334032d17dae5242c2e5
prathameshr04 <125074697+prathameshr04@users.noreply.github.com>
2026-06-28 15:39:35 +0530

    Modified commits

--- app.py
+++ app.py
@@ -3,23 +3,23 @@ import requests
 
 app = Flask(__name__)  #intialize object to class flask
 
-API_Key = "api key"
+API_Key = "c3ba553a1debd0f0b4fc926bbcac2cef"
 
-@app.route("/", methods=["GET","POST"])
-def home():
-    weather = None
+@app.route("/", methods=["GET","POST"]) # to get and post user data from forms(Html) an it is a decoretor
+def home():# default home function
+    weather = None# we do not have any weather data
 
-    if request.method == "POST":
-        city = request.form["city"]
+    if request.method == "POST": #if user search a city
+        city = request.form["city"]# to get city data from the forms
         print("City Entered:", city)
-        url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_Key}&units=metric"
-        response = requests.get(url)
+        url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_Key}&units=metric" #api url
+        response = requests.get(url)# to store response
         print("Status Code:", response.status_code)
         print(response.text)
 
-        if response.status_code == 200:
-            data = response.json()
-
+        if response.status_code == 200:# if status code is sucesseful then
+            data = response.json()#get the data from the api in string fromat 
+            # amking a different dictionary to  only get percise data
             weather = {
                 "city": city,
                 "temp": data["main"]["temp"],
@@ -27,7 +27,7 @@ def home():
                 "wind": data["wind"]["speed"],
                 "description": data["weather"][0]["description"]
             }
-    return render_template("index.html" , weather=weather)
+    return render_template("index.html" , weather=weather)# to redirect data to html forms
 
 if __name__ == "__main__":
     app.run(debug=True)
